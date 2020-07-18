cf0b12ce52617bcfb4be454a00b80bae1dfabd1f
AkiraEga <akira.ega@gmail.com>
2020-07-18 16:51:13 +0900

    rename getSelectionInfo to QuickCopyAsMd

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,37 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import OutputSectionConfig from './config';
-import * as ncp from "copy-paste";
+//import * as ncp from "copy-paste";
 import { start } from 'repl';
 import selectionHandler from './selection';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "selection-info" is now active!');
+    console.log('Congratulations, your extension "quick-copy-as-markdown" is now active!');
 
     let config:OutputSectionConfig = new OutputSectionConfig();
     let sh:selectionHandler = new selectionHandler(config);
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.getSelectionInfo', () => {
-        sh.getCodeSelectionInfo();
-    });
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.QuickCopyAsMd.copyToClipboard', () => {
+            sh.getCodeSelectionInfo();
+        })
+    );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.QuickCopyAsMd.enableTraceMode', () => {
+            sh.getCodeSelectionInfo();
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.QuickCopyAsMd.quickMarkInTraceMode', () => {
+            sh.getCodeSelectionInfo();
+        })
+    );
 }
 
 // this method is called when your extension is deactivated

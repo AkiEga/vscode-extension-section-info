2376a502e76644511f4e83d3e2c4c970b6862b12
AkiEga <akira.ega@gmail.com>
2023-08-06 09:17:00 +0900

    make canCmdExec func more silent

--- src/util/fileUtil.ts
+++ src/util/fileUtil.ts
@@ -2,26 +2,26 @@ import { execSync, ExecSyncOptions } from 'child_process';
 import * as fs from 'fs';
 import * as vscode from 'vscode';
 
-export function IsFileExists(filePath:string){
+export function isFileExists(filePath:string){
 	try{
 		fs.statSync(filePath);
 		return true;
 	}catch(err){
 		if(err === 'ENOENT'){
-			return false
+			return false;
 		}
 	}
 	return false;
 }
 
-export function CanCmdExec(cmdStr:string):boolean {
+export function canCmdExec(cmdStr:string):boolean {
 	let ret:boolean = true;
 	let options:ExecSyncOptions = {
 		// stdio settings for silent cmd execution
 		//       stdin,   stdout,   stderr
 		// Ref) Child process | Node.js v19.6.1 Documentation
 		// https://nodejs.org/api/child_process.html#optionsstdioef)
-		stdio: ['ignore', 'ignore', 'ignore']
+		stdio: [ 'ignore', 'ignore', null ]
 	};
 	try{
 		execSync(cmdStr, options);
@@ -33,7 +33,7 @@ export function CanCmdExec(cmdStr:string):boolean {
 
 export function getWorkspaceDirs(): vscode.WorkspaceFolder[] {
 	let ret:vscode.WorkspaceFolder[] = [];
-	if (vscode.workspace.workspaceFolders != undefined) {
+	if (vscode.workspace.workspaceFolders !== undefined) {
 		for(let ws of vscode.workspace.workspaceFolders) {
 			ret.push(ws);
 		}
@@ -44,10 +44,11 @@ export function getWorkspaceDirs(): vscode.WorkspaceFolder[] {
 export function getWorkspaceDirPath(): string {
 	let ret: string = "";
 	let editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
-	if (undefined != editor) {
-		let workspaceFolder: vscode.WorkspaceFolder | undefined  = vscode.workspace.getWorkspaceFolder(editor.document.uri)
+	if (undefined !== editor) {
+		let workspaceFolder: vscode.WorkspaceFolder | undefined;
+		workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
 
-		if((undefined != workspaceFolder) && (IsFileExists(workspaceFolder.uri.fsPath))) {
+		if((undefined !== workspaceFolder) && (isFileExists(workspaceFolder.uri.fsPath))) {
 			ret = workspaceFolder.uri.fsPath;
 		}
 	}
